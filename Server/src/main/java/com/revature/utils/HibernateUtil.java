c8ae7f47242301357347b333513d666f5d9d8844
ElderFuthark <40003119+ElderFuthark@users.noreply.github.com>
2019-10-09 10:07:15 -0400

    Removed automatic annotated class adder function
    The private function at the bottom of the HibernateUtil file has been
    removed. This change was made purely for testing purposes. Once Spring
    is implemented, HibernateUtil may be obsolete. For now, the classes are
    manually added to the config.

--- Server/src/main/java/com/revature/utils/HibernateUtil.java
+++ Server/src/main/java/com/revature/utils/HibernateUtil.java
@@ -1,15 +1,16 @@
 package com.revature.utils;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.util.ArrayList;
-import java.util.List;
-
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
+
+import com.revature.models.Account;
+import com.revature.models.Channel;
+import com.revature.models.Message;
+import com.revature.models.MessageBoard;
+import com.revature.models.Status;
+import com.revature.models.User;
 
 public class HibernateUtil {
 
@@ -17,7 +18,7 @@ public class HibernateUtil {
 	private static SessionFactory sessionFactory;
 
 	// Create singleton for sessionFactory
-	private static SessionFactory createSessionFactory() {
+	private static SessionFactory buildSessionFactory() {
 
 		/*
 		 * Use hibernate.cf.xml to create a new config and then return the Session
@@ -28,7 +29,12 @@ public class HibernateUtil {
 			log.info("Configuring hibernate");
 			Configuration config = new Configuration();
 			config.configure("hibernate.cfg.xml");
-			addAnnotatedClasses(config);
+			config.addAnnotatedClass(Account.class);
+			config.addAnnotatedClass(Channel.class);
+			config.addAnnotatedClass(Message.class);
+			config.addAnnotatedClass(MessageBoard.class);
+			config.addAnnotatedClass(Status.class);
+			config.addAnnotatedClass(User.class);
 			return config.buildSessionFactory();
 		} catch (Exception e) {
 			log.error("Exceoption raised while configuring hibernate.", e);
@@ -38,37 +44,6 @@ public class HibernateUtil {
 
 	// This is a singleton. There should only be one instance per application.
 	public static SessionFactory getSessionFactory() {
-		return (sessionFactory == null) ? sessionFactory = createSessionFactory() : sessionFactory;
-	}
-
-	/*
-	 * Iterates through class files of specified directory and maps those which
-	 * describe hibernate entities to the config file.
-	 */
-
-	private static void addAnnotatedClasses(Configuration config) throws Exception {
-		log.info("Adding annotated classes.");
-		@SuppressWarnings("rawtypes")
-		List<Class> classPaths = new ArrayList<>();
-		File[] files = new File("./src/main/java/com.revature/models").listFiles();
-
-		for (File file : files) {
-			BufferedReader reader = new BufferedReader(new FileReader(file));
-			String line = reader.readLine();
-			log.info("Looking at " + file.getName());
-			while (line != null) {
-				if (line.contains("@Entity")) {
-					String className = file.getName().substring(0, file.getName().length() - 5);
-					classPaths.add(Class.forName("com.revature.models" + className));
-					log.info("Added " + file.getName() + " to the list of valid annotated classes.");
-					break;
-				}
-				line = reader.readLine();
-			}
-			reader.close();
-		}
-		log.info("Calling addAnnotatedClass on valid class files.");
-		classPaths.forEach(entityClass -> config.addAnnotatedClass(entityClass));
-		log.info("All annotated classes added.");
+		return (sessionFactory == null) ? sessionFactory = buildSessionFactory() : sessionFactory;
 	}
 }
